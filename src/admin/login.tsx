bc96b38433e4f943135be875dd2618130f9304f0
st-linhnguyen <linh.nguyen@supremetech.vn>
2024-06-20 08:41:54 +0700

    initialize

---
+++ src/admin/login.tsx
@@ -0,0 +1,12 @@
+import LoginPage from '@medusajs/admin-ui/ui/src/pages/login';
+
+const MyLogin = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <LoginPage />
+    </div>
+  );
+};
+
+export default MyLogin;
